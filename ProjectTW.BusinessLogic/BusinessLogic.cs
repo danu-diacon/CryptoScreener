79ce2e976fdf5d7588e2bd5e5b1d8491a4d7b3bd
danu-diacon <danu.diacon@gmail.com>
2024-03-05 09:38:03 +0200

    Am adaugat paternu Business Logic

---
+++ ProjectTW.BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,18 @@
+﻿using ProjectTW.BusinessLogic.Interfaces;
+using ProjectTW.BusinessLogic.MainBL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTW.BusinessLogic
+{
+     public class BusinessLogic
+     {
+          public ISession GetSessionBL()
+          {
+               return new SessionBL();
+          }
+     }
+}
---
+++ ProjectTW.Domain/Entities/User/ULoginData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTW.Domain.Entities.User
+{
+     public class ULoginData
+     {
+          public string Credential { get; set; }
+
+          public string Password { get; set; }
+
+          public string Ip { get; set; }
+
+          public DateTime LoginTime { get; set; }
+
+
+     }
+}
