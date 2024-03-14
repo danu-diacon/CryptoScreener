8c66cde63d35aa810d7a6a606f464abb24cd4535
Sirbu Nicolae <nicksirbu0001@gmail.com>
2024-03-14 11:35:23 +0200

    am adaugat patient edit,patient all,patient add

---
+++ ProjectTW.Web/Controllers/AllPatientController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectTW.Web.Controllers
+{
+    public class AllPatientController : Controller
+    {
+        // GET: AllPatient
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjectTW.Web/Controllers/PatientAddController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectTW.Web.Controllers
+{
+    public class PatientAddController : Controller
+    {
+        // GET: PatientAdd
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
