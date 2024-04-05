5678bbedb35f32e20406e448bb239e32a997f9d4
Diacon Danu <danu.diacon@gmail.com>
2024-04-05 22:06:52 +0300

    Am conectat baza de date

---
+++ ProjectTW.Domain/Entities/User/UserDbTable.cs
@@ -0,0 +1,38 @@
+﻿using ProjectTW.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTW.Domain.Entities.User
+{
+    public class UserDbTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Username cannot be longer than 30 characters")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Password cannot be shorter than 8 characteres")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        [StringLength(30)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime LastLogin { get; set; }
+
+        public UserRole Level { get; set; }
+    }
+}
