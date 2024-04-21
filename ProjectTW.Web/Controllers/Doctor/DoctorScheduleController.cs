ebb7972d7437fea498ed329a94ec6aae3c88d916
Leahu Dumitru <d2leahu2003@gmail.com>
2024-04-21 13:47:59 +0300

    Am adaugat pagina de Doctor Schedule si BookAppointment

---
+++ ProjectTW.Web/Controllers/Doctor/DoctorScheduleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectTW.Web.Controllers.Doctor
+{
+    public class DoctorScheduleController : Controller
+    {
+        // GET: DoctorSchedule
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
